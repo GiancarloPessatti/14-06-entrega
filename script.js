94d46ef1f983963f69b34485efd6b7824efd59d6
Giancarlo <giancarlopessatti@hotmail.com>
2022-06-14 11:37:00 -0300

    ver 1

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = "Jhon"
+let sobrenome = "Doyle"
+let anoNascimento = 1991
+let idade =  2022-anoNascimento
+let cidade = "Curitiba"
+let resultado = `Eu sou o ${nome} ${sobrenome}, tenho ${2022-anoNascimento} anos e moro em ${cidade}`
+console.log (resultado)
